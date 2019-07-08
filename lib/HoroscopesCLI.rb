57a1258185708b70619598f90fc17cab3b01794a
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-08 01:20:32 +0000

    first commit

---
+++ lib/HoroscopesCLI.rb
@@ -0,0 +1,6 @@
+require "HoroscopesCLI/version"
+
+module HoroscopesCLI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/HoroscopesCLI/version.rb
@@ -0,0 +1,3 @@
+module HoroscopesCLI
+  VERSION = "0.1.0"
+end
